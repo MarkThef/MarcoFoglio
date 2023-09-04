ec4644f659341ded01997ebe9021c7a9b3ce85b1
MarkThef <124147731+MarkThef@users.noreply.github.com>
2023-09-04 18:35:50 +0200

    Esercizio

---
+++ Modulo 4/D1.py
@@ -0,0 +1,30 @@
+# ESERICZIO 1
+
+studenti = 25
+
+# ESERCIZIO 2
+
+studenti = 25
+print(studenti)
+
+# ESERCIZIO 3
+
+studenti = 25
+nuovi_studenti = 3
+
+# ESERCIZIO 4
+
+studenti = 25
+nuovi_studenti = 3
+totale_studenti = studenti + nuovi_studenti
+print(totale_studenti)
+
+# ESERCIZIO 5
+
+lista_numeri =[0,1,2,3,4,5]
+print(lista_numeri)
+
+# ESERCIZIO 6
+
+lista_film =['Nightmare on elm street','Interstellar','Django']
+print(lista_film)
