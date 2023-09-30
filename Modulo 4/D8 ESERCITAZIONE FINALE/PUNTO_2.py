4730ad55d5150d644f2246283ccfdabc8ffb36f8
MarkThef <124147731+MarkThef@users.noreply.github.com>
2023-09-30 18:19:18 +0200

    Esercizio

--- Modulo 4/D8 ESERCITAZIONE FINALE/PUNTO_2.py
+++ Modulo 4/D8 ESERCITAZIONE FINALE/PUNTO_2.py
@@ -5,10 +5,20 @@ import pandas as pd
 # Carica il file CSV in un DataFrame
 df = pd.read_csv('covid.csv')
 
-# Raggruppa il DataFrame per continente e trova il massimo valore di total_cases in ciascun gruppo
-massimo_per_continente = df.groupby('continent')['total_cases'].max()
-massimo_per_continente = massimo_per_continente.apply(lambda x: "{:,.0f}".format(x))  # Formatta il numero senza decimali e con separatore delle migliaia
+# Raggruppa il DataFrame per continente e location, quindi trova il massimo valore di total_cases in ciascun gruppo
+massimo_per_continente_location = df.groupby(['continent', 'location'])['total_cases'].max().reset_index()
 
-# Stampa il massimo valore di total_cases per ciascun continente
+# Poi, raggruppa nuovamente per continente e somma i massimi valori di total_cases per ciascun continente
+somma_massimi_per_continente = massimo_per_continente_location.groupby('continent')['total_cases'].sum().reset_index()
+
+# Formatta il numero senza decimali e con separatore delle migliaia
+somma_massimi_per_continente['total_cases'] = somma_massimi_per_continente['total_cases'].apply(lambda x: "{:,.0f}".format(x))
+
+# Rinomina la colonna per la somma
+somma_massimi_per_continente = somma_massimi_per_continente.rename(columns={'total_cases': 'Totale Casi'})
+
+# Stampa il risultato
 print('\nTOTALE CASI PER CONTINENTE\n')
-print(massimo_per_continente)
+print(somma_massimi_per_continente)
+
+
--- Modulo 4/D8 ESERCITAZIONE FINALE/PUNTO_3.py
+++ Modulo 4/D8 ESERCITAZIONE FINALE/PUNTO_3.py
@@ -6,58 +6,60 @@ import pandas as pd
 pd.options.display.float_format = '{:,.0f}'.format
 
 def confronto_statistiche_continenti():
+    continenti_validi = ['Africa', 'Asia', 'Europe', 'North america', 'Oceania', 'South america']
 
-    continenti = ['Africa', 'Asia', 'Europe', 'North america', 'Oceania', 'South america']
-
-    dataset = input("\nInserisci il nome del dataset (PER L'ESERCITAZIONE EPICODE INSERIRE IL FILE ==> covid.csv): ").lower()
-    continente1 = input('\nInserisci il nome del primo continente (Africa <> Asia <> Europe <> North america <> Oceania <> South america): ').capitalize()
-    continente2 = input('\nInserisci il nome del secondo continente (Africa <> Asia <> Europe <> North america <> Oceania <> South america): ').capitalize()
+    dataset = input("\nInserisci il nome del dataset (per l'esercitazione, inserire il file 'covid.csv'): ").strip().lower()
+    continente1 = input('\nInserisci il nome del primo continente tra ==> Africa, Asia, Europe, North America, Oceania, South America: ').strip().capitalize()
+    continente2 = input('\nInserisci il nome del secondo continente tra  ==> Africa, Asia, Europe, North America, Oceania, South America: ').strip().capitalize()
     
+    if dataset != 'covid.csv' or continente1 not in continenti_validi or continente2 not in continenti_validi:
+        print('ERRORE! Inserire dati corretti.')
+        return
 
-    if dataset == 'covid.csv' and continente1 in continenti and continente2 in continenti:
+    # Carica il dataset da un file CSV
+    df = pd.read_csv('covid.csv')
 
-        # Carica il dataset da un file CSV
-        df = pd.read_csv('covid.csv')
+    # Filtra solo le righe per i continenti specificati
+    continente_data1 = df[df['continent'] == continente1].copy()
+    continente_data2 = df[df['continent'] == continente2].copy()
 
-        # Filtra solo le righe per i continenti specificati
-        continente_data1 = df[df['continent'] == continente1].copy()
-        continente_data2 = df[df['continent'] == continente2].copy()
+    # Reimposta le opzioni di formattazione per Pandas per visualizzare interi
+    pd.options.display.float_format = '{:,.0f}'.format
 
-        # Filtra solo la colonna 'total_cases'
-        casi_totali1 = continente_data1['total_cases']
-        casi_totali2 = continente_data2['total_cases']
-        mondo_data = df['total_cases']
-        massimi_casi_per_continente = df.groupby('continent')['total_cases'].max()
-        somma_massimi_casi = massimi_casi_per_continente.sum()
+    # Filtra solo la colonna 'total_cases'
+    casi_totali1 = continente_data1.groupby('location')['total_cases'].max()
+    casi_totali2 = continente_data2.groupby('location')['total_cases'].max()
 
+    # Calcola e stampa solo il valore minimo, massimo, media per entrambi i continenti
+    statistica1 = casi_totali1.describe()[['min', 'max', 'mean', '75%']]
+    statistica2 = casi_totali2.describe()[['min', 'max', 'mean', '75%']]
+    statistica_mondo = df['total_cases'].describe()[['min', 'max', 'mean', '75%']]
 
-        # Calcola e stampa solo il valore minimo, massimo, media per entrambi i continenti
-        statistica1 = casi_totali1.describe()[['min', 'max', 'mean', '75%']]
-        statistica2 = casi_totali2.describe()[['min', 'max', 'mean', '75%']]
-        statistica_mondo = mondo_data.describe()[['min', 'max', 'mean', '75%']]
+    # Calcola la somma dei casi per il continente 1
+    somma_casi_continente1 = casi_totali1.sum()
 
+    # Calcola la somma dei casi per il continente 2
+    somma_casi_continente2 = casi_totali2.sum()
 
-        # Calcola le somme dei casi per i continenti
-        somma_casi1 = casi_totali1.max()
-        somma_casi2 = casi_totali2.max()
-        somma_mondo = somma_massimi_casi
+    # Calcola la somma dei casi mondiali
+    somma_casi_mondo = df.groupby('location')['total_cases'].max().sum()
 
-        # Calcola le percentuali rispetto ai casi mondiali
-        percentuale1 = (somma_casi1 / somma_mondo) * 100
-        percentuale2 = (somma_casi2 / somma_mondo) * 100
+    # Calcola le percentuali rispetto ai casi mondiali
+    percentuale1 = (somma_casi_continente1 / somma_casi_mondo) * 100
+    percentuale2 = (somma_casi_continente2 / somma_casi_mondo) * 100
 
-        print(f"\n\nSTATISTICHE PER {continente1}:\n\n{statistica1}\n")
-        print(f"STATISTICHE PER {continente2}:\n\n{statistica2}\n")
-        print(f"STATISTICHE MONDO:\n\n{statistica_mondo}\n")
+    # Stampa le statistiche
+    print(f"\nSTATISTICHE PER {continente1}:\n\n{statistica1}\n")
+    print(f"STATISTICHE PER {continente2}:\n\n{statistica2}\n")
+    print(f"STATISTICHE MONDO:\n\n{statistica_mondo}\n")
 
-        print(f"Percentuale dei casi di {continente1} rispetto al mondo: {percentuale1:.2f}%")
-        print(f"Percentuale dei casi di {continente2} rispetto al mondo: {percentuale2:.2f}%\n\n")
-        print(f"TOTALE CASI MONDO: {'{:,.0f}'.format(somma_mondo)}")
-
-
-
-        
-    else:
-        print('ERRORE! INSERIRE I DATI CORRETTI')
+    # Stampa le statistiche
+    print(f"\nPercentuale dei casi di {continente1} rispetto al mondo: {percentuale1:.2f}%")
+    print(f"Percentuale dei casi di {continente2} rispetto al mondo: {percentuale2:.2f}%\n\n")
+    print(f"TOTALE CASI MONDO: {'{:,.0f}'.format(somma_casi_mondo)}")
 
 confronto_statistiche_continenti()
+
+
+
+
